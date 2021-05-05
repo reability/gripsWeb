d9f9a723a4a33268661cd25dec2657e3afb56328
avanurin <avanurin@stream.ru>
2021-05-06 01:48:44 +0300

    rey

--- models/TMessage.py
+++ models/TMessage.py
@@ -23,7 +23,7 @@ class TMessage:
         print(model.post_date)
         print(type(model.post_date))
         return "{date}\n{description}\n\n{url}".format(
-            date=datetime.fromtimestamp(model.post_date).strftime("%m/%d, %H:%M:%S"),
+            date=datetime.fromtimestamp(int(model.post_date)).strftime("%m/%d, %H:%M:%S"),
             description=model.description,
             url=model.original_url
         )
