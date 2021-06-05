2252d08e82c311aab990c1fc82fe42f2c20f4840
avanurin <avanurin@stream.ru>
2021-06-05 20:12:32 +0300

    fix tg bug

--- models/TMessage.py
+++ models/TMessage.py
@@ -11,15 +11,15 @@ class TMessage:
 
     @staticmethod
     def init_from(ticket_model: Ticket.Model):
-        return TMessage(title=ticket_model.title, description=TMessage._description_for(ticket_model))
+        return TMessage(title=ticket_model.original_url, description=TMessage._description_for(ticket_model))
 
     @staticmethod
     def _description_for(model: Ticket.Model):
-        return "{date}\n{description}\n\n{url}".format(
+        return "{date}\n{description}\n\n".format(
             date=datetime.fromtimestamp(int(model.post_date)).strftime("%m/%d, %H:%M:%S"),
-            description=model.description,
+            description=model.title,
             url=model.original_url
         )
 
     def to_str(self):
-        return "* {0} *\n\n{1}".format(self.title, self.description)
+        return "**{0}**\n\n{1}".format(self.title, self.description)
